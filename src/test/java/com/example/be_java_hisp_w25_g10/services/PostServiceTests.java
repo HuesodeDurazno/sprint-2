195e13e82fe525db066a1b00c808edc0cc6f925b
Vanessa0427 <vaneg504020@gmail.com>
2024-02-29 09:58:32 -0500

    Modify Builder

--- src/test/java/com/example/be_java_hisp_w25_g10/services/PostServiceTests.java
+++ src/test/java/com/example/be_java_hisp_w25_g10/services/PostServiceTests.java
@@ -102,205 +102,35 @@ public class PostServiceTests {
     }
     @Test
     public void VerifyListPostDesc(){
+
         //Arrage
-        PostsDto expected = new PostsDto(
-                3,
-                List.of(
-                        new PostDto(
-                                3,
-                                1,
-                                LocalDate.of (2024, 02, 28),
-                                new ProductDto(1,
-                                        "Producto1",
-                                        "Electrónico",
-                                        "MarcaA",
-                                        "Rojo",
-                                        "Notas sobre el producto",
-                                        123,
-                                        29.99)
+        PostsDto expected = Builder.orderedListPostDesc();
+        List<Post> posts = Builder.postNotInOrder();
 
-                        ),
-                        new PostDto(
-                                3,
-                                1,
-                                LocalDate.of (2024, 02, 22),
-                                new ProductDto(1,
-                                        "Producto1",
-                                        "Electrónico",
-                                        "MarcaA",
-                                        "Rojo",
-                                        "Notas sobre el producto",
-                                        123,
-                                        29.99)
-
-                        ),
-                        new PostDto(
-                                3,
-                                1,
-                                LocalDate.of (2024, 02, 15),
-                                new ProductDto(1,
-                                        "Producto1",
-                                        "Electrónico",
-                                        "MarcaA",
-                                        "Rojo",
-                                        "Notas sobre el producto",
-                                        123,
-                                        29.99)
-
-                        )
-                )
-        );
-
-        List<Post> posts = new ArrayList<>(Arrays.asList(new Post[]{
-
-                new Post(1,
-                        new User(3, "user3", "lastName", RolEnum.BUYER),
-                        LocalDate.of (2024, 02, 15),
-                        new Product(1,
-                                123,
-                                29.99,
-                                "Producto1",
-                                "Electrónico",
-                                "MarcaA",
-                                "Rojo",
-                                "Notas sobre el producto"
-                        )
-                ),
-                new Post(1,
-                        new User(3, "user3", "lastName", RolEnum.BUYER),
-                        LocalDate.of (2024, 02, 28),
-                        new Product(1,
-                                123,
-                                29.99,
-                                "Producto1",
-                                "Electrónico",
-                                "MarcaA",
-                                "Rojo",
-                                "Notas sobre el producto"
-                        )
-                ),
-                new Post(1,
-                        new User(3, "user3", "lastName", RolEnum.BUYER),
-                        LocalDate.of (2024, 02, 22),
-                        new Product(1,
-                                123,
-                                29.99,
-                                "Producto1",
-                                "Electrónico",
-                                "MarcaA",
-                                "Rojo",
-                                "Notas sobre el producto"
-                        )
-                )
-
-        }));
         int userId = 3;
         String sortOrder = "date_desc";
+
         //act
         when(repository.getFollowedPosts(userId)).thenReturn(posts);
         PostsDto actual = service.getPostsFollowed(userId, sortOrder);
+
         //assert
         Assertions.assertEquals(expected, actual);
     }
     @Test
     public void VerifyListPostAsc(){
+
         //Arrage
-        PostsDto expected = new PostsDto(
-                3,
-                List.of(
-                        new PostDto(
-                                3,
-                                1,
-                                LocalDate.of (2024, 02, 15),
-                                new ProductDto(1,
-                                        "Producto1",
-                                        "Electrónico",
-                                        "MarcaA",
-                                        "Rojo",
-                                        "Notas sobre el producto",
-                                        123,
-                                        29.99)
+        PostsDto expected = Builder.orderedListPostAsc();
+        List<Post> posts = Builder.postNotInOrder();
 
-                        ),
-                        new PostDto(
-                                3,
-                                1,
-                                LocalDate.of (2024, 02, 22),
-                                new ProductDto(1,
-                                        "Producto1",
-                                        "Electrónico",
-                                        "MarcaA",
-                                        "Rojo",
-                                        "Notas sobre el producto",
-                                        123,
-                                        29.99)
-
-                        ),
-                        new PostDto(
-                                3,
-                                1,
-                                LocalDate.of (2024, 02, 28),
-                                new ProductDto(1,
-                                        "Producto1",
-                                        "Electrónico",
-                                        "MarcaA",
-                                        "Rojo",
-                                        "Notas sobre el producto",
-                                        123,
-                                        29.99)
-
-                        )
-                )
-        );
-
-        List<Post> posts = new ArrayList<>(Arrays.asList(new Post[]{
-
-                new Post(1,
-                        new User(3, "user3", "lastName", RolEnum.BUYER),
-                        LocalDate.of (2024, 02, 15),
-                        new Product(1,
-                                123,
-                                29.99,
-                                "Producto1",
-                                "Electrónico",
-                                "MarcaA",
-                                "Rojo",
-                                "Notas sobre el producto"
-                        )
-                ),
-                new Post(1,
-                        new User(3, "user3", "lastName", RolEnum.BUYER),
-                        LocalDate.of (2024, 02, 28),
-                        new Product(1,
-                                123,
-                                29.99,
-                                "Producto1",
-                                "Electrónico",
-                                "MarcaA",
-                                "Rojo",
-                                "Notas sobre el producto"
-                        )
-                ),
-                new Post(1,
-                        new User(3, "user3", "lastName", RolEnum.BUYER),
-                        LocalDate.of (2024, 02, 22),
-                        new Product(1,
-                                123,
-                                29.99,
-                                "Producto1",
-                                "Electrónico",
-                                "MarcaA",
-                                "Rojo",
-                                "Notas sobre el producto"
-                        )
-                )
-
-        }));
         int userId = 3;
         String sortOrder = "date_asc";
+
         //act
         when(repository.getFollowedPosts(userId)).thenReturn(posts);
         PostsDto actual = service.getPostsFollowed(userId, sortOrder);
+
         //assert
         Assertions.assertEquals(expected, actual);
     }
--- src/test/java/com/example/be_java_hisp_w25_g10/utils/Builder.java
+++ src/test/java/com/example/be_java_hisp_w25_g10/utils/Builder.java
@@ -1,5 +1,8 @@
 package com.example.be_java_hisp_w25_g10.utils;
 
+import com.example.be_java_hisp_w25_g10.dtos.PostDto;
+import com.example.be_java_hisp_w25_g10.dtos.PostsDto;
+import com.example.be_java_hisp_w25_g10.dtos.ProductDto;
 import com.example.be_java_hisp_w25_g10.entities.Post;
 import com.example.be_java_hisp_w25_g10.entities.Product;
 import com.example.be_java_hisp_w25_g10.entities.RolEnum;
@@ -11,14 +14,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Builder {
-    public static User[] VerifyCountTestBuilder(){
+    public static User[] VerifyCountTestBuilder() {
         return new User[]{
                 new User(1, "user1", "lastName", RolEnum.SELLER),
                 new User(3, "user3", "lastName", RolEnum.BUYER),
         };
     }
 
-    public static List<Post> testSortExistBuilder(){
+    public static List<Post> testSortExistBuilder() {
         return new ArrayList<>(Arrays.asList(new Post[]{
                 new Post(1,
                         new User(1, "user1", "lastName", RolEnum.SELLER),
@@ -48,4 +51,154 @@ public class Builder {
                 )
         }));
     }
+
+    //Ordered List in a Desc way
+    public static PostsDto orderedListPostDesc() {
+        LocalDate today = LocalDate.now();
+        PostsDto orderDescList = new PostsDto(
+                3,
+                List.of(
+                        new PostDto(
+                                3,
+                                1,
+                                today.plusDays(1),
+                                new ProductDto(1,
+                                        "Producto1",
+                                        "Electrónico",
+                                        "MarcaA",
+                                        "Rojo",
+                                        "Notas sobre el producto",
+                                        123,
+                                        29.99)
+
+                        ),
+                        new PostDto(
+                                3,
+                                1,
+                                today,
+                                new ProductDto(1,
+                                        "Producto1",
+                                        "Electrónico",
+                                        "MarcaA",
+                                        "Rojo",
+                                        "Notas sobre el producto",
+                                        123,
+                                        29.99)
+
+                        ),
+                        new PostDto(
+                                3,
+                                1,
+                                today.minusDays(1),
+                                new ProductDto(1,
+                                        "Producto1",
+                                        "Electrónico",
+                                        "MarcaA",
+                                        "Rojo",
+                                        "Notas sobre el producto",
+                                        123,
+                                        29.99)
+
+                        )
+                )
+        );
+        return orderDescList;
+    }
+
+    //Ordered List in an Asc way
+    public static PostsDto orderedListPostAsc () {
+        LocalDate today = LocalDate.now();
+        PostsDto orderAscList = new PostsDto(3,
+                List.of(
+                    new PostDto(
+                            3,
+                            1,
+                            today.minusDays(1),
+                            new ProductDto(1,
+                                    "Producto1",
+                                    "Electrónico",
+                                    "MarcaA",
+                                    "Rojo",
+                                    "Notas sobre el producto",
+                                    123,
+                                    29.99)
+                    ),
+                    new PostDto(
+                            3,
+                            1,
+                            today,
+                            new ProductDto(1,
+                                    "Producto1",
+                                    "Electrónico",
+                                    "MarcaA",
+                                    "Rojo",
+                                    "Notas sobre el producto",
+                                    123,
+                                    29.99)
+                    ),
+                    new PostDto(
+                            3,
+                            1,
+                            today.plusDays(1),
+                            new ProductDto(1,
+                                    "Producto1",
+                                    "Electrónico",
+                                    "MarcaA",
+                                    "Rojo",
+                                    "Notas sobre el producto",
+                                    123,
+                                    29.99)
+                    )
+                )
+        );
+        return orderAscList;
+    }
+//Posts ready to be ordered
+    public static List<Post> postNotInOrder () {
+        LocalDate today = LocalDate.now();
+        List<Post> posts = new ArrayList<>(Arrays.asList(new Post[]{
+                new Post(1,
+                        new User(3, "user3", "lastName", RolEnum.BUYER),
+                        today.minusDays(1),
+                        new Product(1,
+                                123,
+                                29.99,
+                                "Producto1",
+                                "Electrónico",
+                                "MarcaA",
+                                "Rojo",
+                                "Notas sobre el producto"
+                        )
+                ),
+                new Post(1,
+                        new User(3, "user3", "lastName", RolEnum.BUYER),
+                        today.plusDays(1),
+                        new Product(1,
+                                123,
+                                29.99,
+                                "Producto1",
+                                "Electrónico",
+                                "MarcaA",
+                                "Rojo",
+                                "Notas sobre el producto"
+                        )
+                ),
+                new Post(1,
+                        new User(3, "user3", "lastName", RolEnum.BUYER),
+                        today,
+                        new Product(1,
+                                123,
+                                29.99,
+                                "Producto1",
+                                "Electrónico",
+                                "MarcaA",
+                                "Rojo",
+                                "Notas sobre el producto"
+                        )
+                )
+
+        }));
+        return posts;
+    }
+
 }
